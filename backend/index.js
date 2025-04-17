618c48fe34aaaefbdc0695dcaaea40b11419f965
yash lalage <yashlalage@gmail.com>
2025-04-17 23:45:03 +0530

    frontend init

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    return res.json({
-        "msg" : "Home page"
-    })
-})
+app.get("/", (req, res) => {
+  return res.json({
+    msg: "Home page",
+  });
+});
 
-app.listen(port,(req,res)=>{
-    console.log(`App listning on port ${port}`);
-})
+app.listen(port, (req, res) => {
+  console.log(`App listning on port ${port}`);
+});
